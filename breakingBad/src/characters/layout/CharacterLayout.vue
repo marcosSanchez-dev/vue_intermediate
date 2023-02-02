b77fc0c3d37363889d7ace98eb0613aacb07e400
Marcos <marcos.sanchez@saei.mx>
2023-02-02 12:04:57 -0600

    Directory setup

---
+++ breakingBad/src/characters/layout/CharacterLayout.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>CharacterLayout inside <b>Characters</b></h1>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ breakingBad/src/shared/pages/AboutPage.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>AboutPage inside <b>Shared</b></h1>
+  </div>
+</template>
+
+<style scoped></style>
